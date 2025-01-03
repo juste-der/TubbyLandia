ec679cf67fb36111004c66df847b90331cd69a2a
Juste <juste.derk@gmail.com>
2025-01-03 18:12:04 +0100

    Initialised hero banner and started on the main page

--- index.php
+++ index.php
@@ -1,1 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aclonica&family=Bebas+Neue&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=IBM+Plex+Sans+JP&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lexend+Deca:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <img src="/images/logo.svg" alt="">
+        <h1>SUNSHINE DOME HOTEL</h1>
+        <h3>TubbyLandia</h3>
+    </header>
+    <section class="hero-container">
+        <div class="slider-wrapper">
+            <div class="slider">
+                <img id="slide-1" src="/images/hero1.avif" alt="Hero slider image 1">
+                <img id="slide-2" src="/images/hero2.avif" alt="Hero slider image 2">
+                <img id="slide-3" src="/images/hero3.avif" alt="Hero slider image 3">
+                <img id="slide-4" src="/images/hero4.avif" alt="Hero slider image 4">
+                <img id="slide-5" src="/images/hero5.avif" alt="Hero slider image 5">
+            </div>
+            <div class="slider-nav">
+                <a href="#slide-1"></a>
+                <a href="#slide-2"></a>
+                <a href="#slide-3"></a>
+                <a href="#slide-4"></a>
+                <a href="#slide-5"></a>
+            </div>
+        </div>
+    </section>
+    <h1>Feel the Tubby Magic – Your Perfect Escape Awaits!</h1>
+    <div class="room-1">
+        <h1>Luxury Room</h1>
+        <div class="room-img-container">
+            <img src="/images/luxury_room1.webp" alt="Luxury room image 1">
+            <button class="image-button">
+                <img src="/images/img-button.png" alt="Image container button">
+            </button>
+        </div>
+        <div id="room1-popup" class="room1-popup">
+            <span class="close">&times;</span>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
