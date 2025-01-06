d268dc804bd1e8605988b9c8e5b88cf23cdf5964
Juste <juste.derk@gmail.com>
2025-01-06 19:29:32 +0100

    Deploying issue

--- index.php
+++ index.php
@@ -13,18 +13,18 @@
 
 <body>
     <header class="header">
-        <img src="/images/logo.svg" alt="">
+        <img src="images/logo.svg" alt="">
         <h1>SUNSHINE DOME HOTEL</h1>
         <h3>TubbyLandia</h3>
     </header>
     <section class="hero-container">
         <div class="slider-wrapper">
             <div class="slider">
-                <img id="slide-1" src="/images/hero1.avif" alt="Hero slider image 1">
-                <img id="slide-2" src="/images/hero2.avif" alt="Hero slider image 2">
-                <img id="slide-3" src="/images/hero3.avif" alt="Hero slider image 3">
-                <img id="slide-4" src="/images/hero4.avif" alt="Hero slider image 4">
-                <img id="slide-5" src="/images/hero5.avif" alt="Hero slider image 5">
+                <img id="slide-1" src="images/hero1.avif" alt="Hero slider image 1">
+                <img id="slide-2" src="images/hero2.avif" alt="Hero slider image 2">
+                <img id="slide-3" src="images/hero3.avif" alt="Hero slider image 3">
+                <img id="slide-4" src="images/hero4.avif" alt="Hero slider image 4">
+                <img id="slide-5" src="images/hero5.avif" alt="Hero slider image 5">
             </div>
             <div class="slider-nav">
                 <a href="#slide-1"></a>
@@ -43,9 +43,9 @@
             <div class="left-container">
                 <p class="room-title">Luxury Room</p>
                 <div class="gallery-container">
-                    <img src="/images/luxury_room1.webp" alt="Luxury room image 1">
+                    <img src="images/luxury_room1.webp" alt="Luxury room image 1">
                     <button class="image-button">
-                        <img src="/images/img-button.png" alt="Image container button">
+                        <img src="images/img-button.png" alt="Image container button">
                     </button>
                 </div>
                 <div class="gallery-popup">
@@ -53,10 +53,10 @@
                     <div class="gallery-content">
                         <span class="close">&times;</span>
                         <div class="gallery-slider">
-                            <img id="luxury-room1" src="/images/luxury_room1.webp" alt="Luxury room image 1">
-                            <img id="luxury-room2" src="/images/luxury_room2.webp" alt="Luxury room image 2">
-                            <img id="luxury-room3" src="/images/luxury_room3.webp" alt="Luxury room image 3">
-                            <img id="luxury-room4" src="/images/luxury_room4.webp" alt="Luxury room image 4">
+                            <img id="luxury-room1" src="images/luxury_room1.webp" alt="Luxury room image 1">
+                            <img id="luxury-room2" src="images/luxury_room2.webp" alt="Luxury room image 2">
+                            <img id="luxury-room3" src="images/luxury_room3.webp" alt="Luxury room image 3">
+                            <img id="luxury-room4" src="images/luxury_room4.webp" alt="Luxury room image 4">
                         </div>
                         <div class="gallery-slider-nav">
                             <a href="#luxury-room1"></a>
@@ -68,29 +68,29 @@
                 </div>
                 <div class="room-details">
                     <div class="room-detail">
-                        <img src="/images/size.svg" alt="room size icon">
+                        <img src="images/size.svg" alt="room size icon">
                         <p>72 m²</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/person.svg" alt="room person icon">
+                        <img src="images/person.svg" alt="room person icon">
                         <p>4</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/bed.svg" alt="room bed icon">
+                        <img src="images/bed.svg" alt="room bed icon">
                         <p>4 Twin or 2 King</p>
                     </div>
                 </div>
                 <div class="amenities-container">
                     <div class="amenity">
-                        <img src="/images/pool.svg" alt="pool icon">
+                        <img src="images/pool.svg" alt="pool icon">
                         <p>Spa Area <br> with Pool</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/yatzy.svg" alt="yatzy icon">
+                        <img src="images/yatzy.svg" alt="yatzy icon">
                         <p>Yatzy</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/minibar.svg" alt="minibar icon">
+                        <img src="images/minibar.svg" alt="minibar icon">
                         <p>Minibar</p>
                     </div>
                 </div>
@@ -98,7 +98,7 @@
             <div class="right-container">
                 <div class="details-container">
                     <div class="details-left">
-                        <p class="pay-online">Pay Online <img src="/images/card.svg" alt="card icon"></p>
+                        <p class="pay-online">Pay Online <img src="images/card.svg" alt="card icon"></p>
                         <p class="price-info">Price per night</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="68" height="2" viewBox="0 0 68 2" fill="none">
                             <path d="M0 1.5L67.5 1" stroke="#53350B" stroke-opacity="0.2" />
@@ -118,9 +118,9 @@
             <div class="left-container">
                 <p class="room-title">Standard Room</p>
                 <div class="gallery-container">
-                    <img src="/images/standard_room3.webp" alt="Standard room image">
+                    <img src="images/standard_room3.webp" alt="Standard room image">
                     <button class="image-button">
-                        <img src="/images/img-button.png" alt="Image container button">
+                        <img src="images/img-button.png" alt="Image container button">
                     </button>
                 </div>
                 <div class="gallery-popup">
@@ -128,10 +128,10 @@
                     <div class="gallery-content">
                         <span class="close">&times;</span>
                         <div class="gallery-slider">
-                            <img id="standard-room1" src="/images/standard_room1.webp" alt="Standard room image 1">
-                            <img id="standard-room2" src="/images/standard_room2.avif" alt="Standard room image 2">
-                            <img id="standard-room3" src="/images/standard_room3.webp" alt="Standard room image 3">
-                            <img id="standard-room4" src="/images/standard_room4.webp" alt="Standard room image 4">
+                            <img id="standard-room1" src="images/standard_room1.webp" alt="Standard room image 1">
+                            <img id="standard-room2" src="images/standard_room2.avif" alt="Standard room image 2">
+                            <img id="standard-room3" src="images/standard_room3.webp" alt="Standard room image 3">
+                            <img id="standard-room4" src="images/standard_room4.webp" alt="Standard room image 4">
                         </div>
                         <div class="gallery-slider-nav">
                             <a href="#standard-room1"></a>
@@ -143,29 +143,29 @@
                 </div>
                 <div class="room-details">
                     <div class="room-detail">
-                        <img src="/images/size.svg" alt="room size icon">
+                        <img src="images/size.svg" alt="room size icon">
                         <p>52 m²</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/person.svg" alt="room person icon">
+                        <img src="images/person.svg" alt="room person icon">
                         <p>2</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/bed.svg" alt="room bed icon">
+                        <img src="images/bed.svg" alt="room bed icon">
                         <p>2 Twin or 1 King</p>
                     </div>
                 </div>
                 <div class="amenities-container">
                     <div class="amenity">
-                        <img src="/images/pool.svg" alt="pool icon">
+                        <img src="images/pool.svg" alt="pool icon">
                         <p>Spa Area <br> with Pool</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/yatzy.svg" alt="yatzy icon">
+                        <img src="images/yatzy.svg" alt="yatzy icon">
                         <p>Yatzy</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/minibar.svg" alt="minibar icon">
+                        <img src="images/minibar.svg" alt="minibar icon">
                         <p>Minibar</p>
                     </div>
                 </div>
@@ -173,7 +173,7 @@
             <div class="right-container">
                 <div class="details-container">
                     <div class="details-left">
-                        <p class="pay-online">Pay Online <img src="/images/card.svg" alt="card icon"></p>
+                        <p class="pay-online">Pay Online <img src="images/card.svg" alt="card icon"></p>
                         <p class="price-info">Price per night</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="68" height="2" viewBox="0 0 68 2" fill="none">
                             <path d="M0 1.5L67.5 1" stroke="#53350B" stroke-opacity="0.2" />
@@ -193,9 +193,9 @@
             <div class="left-container">
                 <p class="room-title">Budget Room</p>
                 <div class="gallery-container">
-                    <img src="/images/budget_room1.webp" alt="Budget room image">
+                    <img src="images/budget_room1.webp" alt="Budget room image">
                     <button class="image-button">
-                        <img src="/images/img-button.png" alt="Image container button">
+                        <img src="images/img-button.png" alt="Image container button">
                     </button>
                 </div>
                 <div class="gallery-popup">
@@ -203,10 +203,10 @@
                     <div class="gallery-content">
                         <span class="close">&times;</span>
                         <div class="gallery-slider">
-                            <img id="budget-room1" src="/images/budget_room1.webp" alt="budget room image 1">
-                            <img id="budget-room2" src="/images/budget_room2.avif" alt="budget room image 2">
-                            <img id="budget-room3" src="/images/budget_room3.webp" alt="budget room image 3">
-                            <img id="budget-room4" src="/images/budget_room4.webp" alt="budget room image 4">
+                            <img id="budget-room1" src="images/budget_room1.webp" alt="budget room image 1">
+                            <img id="budget-room2" src="images/budget_room2.avif" alt="budget room image 2">
+                            <img id="budget-room3" src="images/budget_room3.webp" alt="budget room image 3">
+                            <img id="budget-room4" src="images/budget_room4.webp" alt="budget room image 4">
                         </div>
                         <div class="gallery-slider-nav">
                             <a href="#budget-room1"></a>
@@ -218,29 +218,29 @@
                 </div>
                 <div class="room-details">
                     <div class="room-detail">
-                        <img src="/images/size.svg" alt="room size icon">
+                        <img src="images/size.svg" alt="room size icon">
                         <p>32 m²</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/person.svg" alt="room person icon">
+                        <img src="images/person.svg" alt="room person icon">
                         <p>1</p>
                     </div>
                     <div class="room-detail">
-                        <img src="/images/bed.svg" alt="room bed icon">
+                        <img src="images/bed.svg" alt="room bed icon">
                         <p> &nbsp; &nbsp; 1 Twin &nbsp;</p>
                     </div>
                 </div>
                 <div class="amenities-container">
                     <div class="amenity">
-                        <img src="/images/pool.svg" alt="pool icon">
+                        <img src="images/pool.svg" alt="pool icon">
                         <p>Spa Area <br> with Pool</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/yatzy.svg" alt="yatzy icon">
+                        <img src="images/yatzy.svg" alt="yatzy icon">
                         <p>Yatzy</p>
                     </div>
                     <div class="amenity">
-                        <img src="/images/minibar.svg" alt="minibar icon">
+                        <img src="images/minibar.svg" alt="minibar icon">
                         <p>Minibar</p>
                     </div>
                 </div>
@@ -248,7 +248,7 @@
             <div class="right-container">
                 <div class="details-container">
                     <div class="details-left">
-                        <p class="pay-online">Pay Online <img src="/images/card.svg" alt="card icon"></p>
+                        <p class="pay-online">Pay Online <img src="images/card.svg" alt="card icon"></p>
                         <p class="price-info">Price per night</p>
                         <svg xmlns="http://www.w3.org/2000/svg" width="68" height="2" viewBox="0 0 68 2" fill="none">
                             <path d="M0 1.5L67.5 1" stroke="#53350B" stroke-opacity="0.2" />
@@ -265,7 +265,7 @@
 
         </div>
     </div>
-    <script src="/script/script.js"></script>
+    <script src="script/script.js"></script>
 </body>
 
 </html>
