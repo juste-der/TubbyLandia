4065ce2538b87eb747aaf8d8c71d8e757ec9cdbe
Juste <juste.derk@gmail.com>
2025-01-10 01:15:13 +0100

    Deploying issue

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('sqlite:tubbylandia.sqlite3');
+    $pdo = new PDO('sqlite:' . __DIR__ . '/tubbylandia.sqlite3');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
